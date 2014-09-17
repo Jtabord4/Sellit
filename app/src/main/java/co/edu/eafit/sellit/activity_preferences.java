c26de2cf064d5be7ae27b846ed14d8aee7add2d0
Felipe Builes <pipeloncho.15@gmail.com>
2014-09-16 21:11:14 -0500

    ahora con login de g+ y sin facebook, porque fu. cambié los strings a español y arreglé ids confusos.

--- app/src/main/java/co/edu/eafit/sellit/activity_preferences.java
+++ app/src/main/java/co/edu/eafit/sellit/activity_preferences.java
@@ -1,32 +1,29 @@
 package co.edu.eafit.sellit;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
-/**
- * Created by Jtabord4 on 9/14/2014.
- */
 public class activity_preferences extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preferences);
 
-        Button agree =(Button)findViewById(R.id.button2);
-
+        Button agree =(Button)findViewById(R.id.btn_preferences_agree);
 
         agree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+
+                updatePreferences(preferences);
+
                 Intent intent_preferences = new Intent(getApplicationContext(),Activity_feed.class);
                 startActivity(intent_preferences);
             }
@@ -34,39 +31,9 @@ public class activity_preferences extends Activity {
 
     }
 
+    public void updatePreferences(SharedPreferences pref) {
 
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_my, container, false);
-            return rootView;
-        }
     }
 }
-
 
 
